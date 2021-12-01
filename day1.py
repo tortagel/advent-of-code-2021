3f25079f252a902e1ebc062bbe0cfc2e82a4b4d6
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-01 08:02:58 +0100

    Add day 1 solution

--- day1.py
+++ day1.py
@@ -1,8 +1,8 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    return [int(n) for n in puzzle]
 
 def solve_part1(puzzle):
-    return None
+    return sum([1 for i, n in enumerate(puzzle[1:]) if n > puzzle[i]])
 
-def solve_part2(puzzle):
-    return None
+def solve_part2(puzzle):    
+    return sum([1 for i in range(len(puzzle)-3) if sum(puzzle[i+1:i+4]) > sum(puzzle[i:i+3])])
