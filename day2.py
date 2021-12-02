24c004cf74ad41a1ae93710aaf4f17c9e658c5b4
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-02 06:16:37 +0100

    Add day 2 solution

--- day2.py
+++ day2.py
@@ -1,8 +1,25 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    return [(x.split(' ')[0], int(x.split(' ')[1])) for x in puzzle]
 
 def solve_part1(puzzle):
-    return None
+    pos = depth = 0
+    for direction, n in puzzle:
+        if direction == 'forward':
+            pos += n
+        elif direction == 'up':
+            depth -= n
+        elif direction == 'down':
+            depth += n
+    return pos * depth
 
 def solve_part2(puzzle):
-    return None
+    pos = depth = aim = 0
+    for direction, n in puzzle:
+        if direction == 'forward':
+            pos += n
+            depth += aim * n
+        elif direction == 'up':
+            aim -= n
+        elif direction == 'down':
+            aim += n
+    return pos * depth
