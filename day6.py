ccdc29ae3b252030a406f62f553e200286b10c2a
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-06 07:56:33 +0100

    Add day 6 solution

--- day6.py
+++ day6.py
@@ -1,8 +1,18 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    return [int(n) for n in puzzle[0].split(',')]
+
+def simulate(puzzle, days):
+    fish = {n: puzzle.count(n) for n in range(9)}
+    for _ in range(days):
+        c0 = fish[0]
+        for n in range(1, 9):
+            fish[n-1] = fish[n]
+        fish[6] += c0
+        fish[8] = c0
+    return sum(fish.values())
 
 def solve_part1(puzzle):
-    return None
+    return simulate(puzzle, 80)
 
 def solve_part2(puzzle):
-    return None
+    return simulate(puzzle, 256)
