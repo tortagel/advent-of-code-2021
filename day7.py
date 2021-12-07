7f5ce1e17b519b3eaf7633055243db5b1f789002
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-07 06:45:01 +0100

    Add day 7 solution

--- day7.py
+++ day7.py
@@ -1,8 +1,9 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    return [int(n) for n in puzzle[0].split(',')]
 
 def solve_part1(puzzle):
-    return None
+    return min([sum([abs(n2 - n1) for n2 in puzzle]) for n1 in range(max(puzzle)+1)])
 
 def solve_part2(puzzle):
-    return None
+    f = lambda n: n * (n + 1) // 2
+    return min([sum([f(abs(n2 - n1)) for n2 in puzzle]) for n1 in range(max(puzzle)+1)])
