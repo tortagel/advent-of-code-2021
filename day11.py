3bcb5f7e8d67892ed2be6cdc2b5ba3c7ed5b942d
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-12 13:00:17 +0100

    Add day 11 solution

--- day11.py
+++ day11.py
@@ -1,8 +1,34 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    return [[int(n) for n in x] for x in puzzle]
+
+adjacent = [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+
+def step(energy):
+    len_x, len_y = len(energy[0]), len(energy)
+    for y in range(len_y):
+        for x in range(len_x):
+            energy[y][x] += 1
+    flashed_octopuses, flashed = set(), True
+    while flashed:
+        flashed = False
+        for y in range(len_y):
+            for x in range(len_x):
+                if energy[y][x] > 9 and (x, y) not in flashed_octopuses:
+                    flashed = True
+                    flashed_octopuses.add((x, y))
+                    for ix, iy in adjacent:
+                        if y+iy >= 0 and y+iy < len_y and x+ix >= 0 and x+ix < len_x:
+                            energy[y+iy][x+ix] += 1
+        for x, y in flashed_octopuses:
+            energy[y][x] = 0
+    return flashed_octopuses
 
 def solve_part1(puzzle):
-    return None
+    return sum([len(step(puzzle)) for _ in range(100)])
 
 def solve_part2(puzzle):
-    return None
+    steps = 0
+    while len({n for line in puzzle for n in line}) > 1:
+        steps += 1
+        step(puzzle)
+    return steps
