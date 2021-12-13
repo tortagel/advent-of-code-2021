9b444e119969aed2d2563417ccb8d7db54369cd5
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-13 18:45:12 +0100

    Add day 13 solution

--- day13.py
+++ day13.py
@@ -1,8 +1,42 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    dots = set()
+    folds = []
+    for x in puzzle:
+        if ',' in x:
+            tmp = x.split(',')
+            dots.add((int(tmp[0]), int(tmp[1])))
+        elif '=' in x:
+            tmp = x.split('fold along ')[1].split('=')
+            folds.append((tmp[0], int(tmp[1])))
+    return (dots, folds)
+
+def fold_paper(dots, fold):
+    direction, line = fold
+    folded = set()
+    for x, y in dots:
+        if direction == 'y':
+            if y > line:
+                folded.add((x, line - (y-line)))
+            else:
+                folded.add((x, y))
+        elif direction == 'x':
+            if x > line:
+                folded.add((line - (x-line), y))
+            else:
+                folded.add((x, y))
+    return folded
 
 def solve_part1(puzzle):
-    return None
+    dots, folds = puzzle
+    return len(fold_paper(dots, folds[0]))
 
 def solve_part2(puzzle):
-    return None
+    dots, folds = puzzle
+    for fold in folds:
+        dots = fold_paper(dots, fold)
+    tmp = [['.' for _ in range(max([x for x, _ in dots])+1)] for _ in range(max([y for _, y in dots])+1)]
+    for x, y in dots:
+        tmp[y][x] = '#'
+    for l in tmp:
+        print(''.join(l))
+    return 'see output before...'
