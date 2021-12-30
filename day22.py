e6ff5c60661ff7e5a1dd35849cfecdf66525f3d3
Michael Reisigl <e11714908@student.tuwien.ac.at>
2021-12-30 13:18:03 +0100

    Add day 22 part 1 solution

--- day22.py
+++ day22.py
@@ -1,8 +1,19 @@
 def prepare_puzzle(puzzle):
-    return puzzle
+    reboot_steps = []
+    for line in puzzle:
+        tmp = line.split(' ')
+        x, y, z = [t[2:].split('..') for t in tmp[1].split(',')]        
+        reboot_steps.append((tmp[0] == 'on', (int(x[0]), int(x[1])), (int(y[0]), int(y[1])), (int(z[0]), int(z[1])) ))
+    return reboot_steps
 
 def solve_part1(puzzle):
-    return None
+    cubes = dict()
+    for on, (x1, x2), (y1, y2), (z1, z2) in puzzle:
+        for x in range(max(x1, -50), min(x2+1, 51)):
+            for y in range(max(y1, -50), min(y2+1, 51)):
+                for z in range(max(z1, -50), min(z2+1, 51)):
+                    cubes[(x, y, z)] = on
+    return len([1 for on in cubes.values() if on])
 
 def solve_part2(puzzle):
     return None
